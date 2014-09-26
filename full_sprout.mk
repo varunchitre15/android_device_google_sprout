3abaaf6b0622f28f7eebc429916461e5d62e5e33
Varun Chitre <varun.chitre15@gmail.com>
2014-09-26 10:41:59 -0400

    sprout: twrp support

--- full_sprout.mk
+++ full_sprout.mk
@@ -4,8 +4,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 # Needed stuff
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
+LOCAL_PATH := device/micromax/sprout
+
 PRODUCT_NAME := cm_sprout
 PRODUCT_DEVICE :=Sprout
 PRODUCT_BRAND := Micromax
 PRODUCT_MANUFACTURER := Micromax
 PRODUCT_MODEL := A1
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/twrp.fstab:recovery/root/etc/twrp.fstab
