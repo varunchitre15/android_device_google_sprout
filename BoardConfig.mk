37c2dbde74044a860fdd3156201f881dda79f28e
Varun Chitre <varun.chitre15@gmail.com>
2014-12-05 09:08:31 +0100

    sprout: use 4.7 toolchain for kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,6 +33,7 @@ TARGET_KERNEL_SOURCE := kernel/mediatek/sprout
 TARGET_KERNEL_CONFIG := cyanogenmod_sprout_defconfig
 
 BOARD_RECOVERY_SWIPE := true
+KERNEL_TOOLCHAIN := $(ARM_EABI_TOOLCHAIN)/../../arm-eabi-4.7/bin
 
 # TWRP stuff
 RECOVERY_GRAPHICS_USE_LINELENGTH := true
